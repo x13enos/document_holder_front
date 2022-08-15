d1253a23d4115a49dc55014c62efaf42c0aa7415
Andres <x3enos@gmail.com>
2022-08-15 12:18:56 +0200

    added spinner, fetch user's data, added user's block to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ function SidebarLink ({ title, icon, link }) {
     <>
       <li>
         <Link to={link} className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">{icon}</span>
+          <span className="inline-flex items-center h-12 w-8 text-lg text-gray-400">{icon}</span>
           <span className="text-sm font-medium">{title}</span>
         </Link>
       </li>
@@ -15,17 +15,27 @@ function SidebarLink ({ title, icon, link }) {
   )
 }
 
-function Sidebar() {
+function Sidebar({ currentUser }) {
   return (
-    <div className="min-h-screen flex flex-row border-r">
-      <div className="flex flex-col w-56 bg-white overflow-hidden">
+    <div className="grid grid-rows-6 w-56 bg-white overflow-hidden border-r min-h-screen">
+      <div className='flex row row-span-5 flex-col'>
         <div className="flex items-center justify-center h-20 shadow-md">
           <h1>Document Holder</h1>
         </div>
-        <ul className="flex flex-col py-4">
+        <ul className="flex flex-col p-4 items-start">
           <SidebarLink link="/" title="Documents" icon={<CgFolder />} />
           <SidebarLink link="/boxes" title="Boxes" icon={<CgBox />} />
         </ul>
+      </div>
+      <div className="row row-span-1 flex p-4 items-end">
+        <div className='flex'>
+          <div className='avatar placeholder'>
+            <div class="bg-neutral-focus text-neutral-content rounded-full w-8">
+              {currentUser.email.slice(0, 2)}
+            </div>
+          </div>
+          <div className='ml-1 mt-1'>{currentUser.email}</div>
+        </div>
       </div>
     </div>
   )
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
+import httpClient from '../axiosConfig';
 
-const MainLayout =() =>{
+const MainLayout = () => {
+  const [currentUser, setCurrentUser] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const fetchUserData = async (params = {}) => {
+    const response = await httpClient.get('/users/current_user_info');
+    setCurrentUser(response.data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchUserData();
+  } , []);
   
   return(
     <>
-      <div className="flex flex-row">
-        <div><Sidebar /></div>
-        <div className='mt-4 w-full'><Outlet /></div>
-      </div>
+      { loading ? 
+        <div className="spinner-container flex justify-center items-center h-screen">
+          <div className="loading-spinner">
+          </div>
+        </div> :
+        <div className="flex flex-row">
+          <div><Sidebar currentUser={currentUser} /></div>
+          <div className='mt-4 w-full'><Outlet /></div>
+        </div>
+      }
+
     </>
   )
 }
